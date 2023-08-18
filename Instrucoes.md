d71bff83b6c9312fe496934a213d8754ae0c5f9f
Gabriel Carvalho <gribeirocarvalho@hotmail.com>
2023-08-18 07:37:30 -0300

    first commit

---
+++ Instrucoes.md
@@ -0,0 +1,10 @@
+# Teste de Performance 2
+## Exercício #1
+Implemente um programa no qual o usuário informe o nome e a data de nascimento de três amigos(as). Ao fim, o programa deve exibir o nome e a idade do(a) amigo(a) mais velho(a).
+
+Requisitos técnicos:
+
+Crie uma classe para representar amigo;
+Crie o construtor na classe (item 1) para definir o nome e a data de nascimento do amigo;
+Implemente no programa principal a criação de três amigos (dados reais ou fictícios);
+Utilize a classe Console apenas no programa principal;
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Amigo
+{
+    public string nome {get; set;}
+    public int dia {get; set;}
+    public int mes {get; set;}
+    public int ano {get; set;}
+
+    public var aniversario = new DateTime(ano, mes, dia);
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {   
+        //Construção do amigo 1
+        Amigo amigo1 = new Amigo();
+
+        Console.Write("Digite o nome do seu primeiro amigo: ");
+        string nome1 = Console.ReadLine();
+        amigo1.nome = nome1;
+        Console.WriteLine($"O nome captado foi {amigo1.nome}");
+
+        Console.Write("Digite o dia de nascimento do seu primeiro amigo: ");
+        string diaString1 = Console.ReadLine();
+        amigo1.dia = int.Parse(diaString1);
+        Console.WriteLine($"O dia captado foi {amigo1.dia}");
+
+        Console.Write("Digite o mês de nascimento do seu primeiro amigo: ");
+        string mesString1 = Console.ReadLine();
+        amigo1.mes = int.Parse(mesString1);
+        Console.WriteLine($"O mês captado foi {amigo1.mes}");
+
+        Console.Write("Digite o ano de nascimento do seu primeiro amigo: ");
+        string anoString1 = Console.ReadLine();
+        amigo1.ano = int.Parse(anoString1);
+        Console.WriteLine($"O ano captado foi {amigo1.ano}");
+
+        
+        
+        //var dia1 = int.Parse(diaString1);
+        //var mes1 = int.Parse(mesString1);
+        //var ano1 = int.Parse(anoString1);      
+    }
+}
